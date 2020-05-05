cbbe986a8316432da69e12359415ed4d28dacc1e
Ilya Skrypnik <ilyaskrypnik@gmail.com>
2020-05-05 21:24:27 +0700

    Add publication form for POST method

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,34 @@
 
 let publicationApi = Vue.resource('/publication{/id}');
 
+Vue.component('publication-form', {
+    props: ['publications'],
+    data: function() {
+        return {
+            name: '',
+            text: ''
+        }
+    },
+    template:
+        '<div>' +
+            '<input type="text" placeholder="Write publication name" v-model="name"/>' +
+            '<input type="text" placeholder="Write publication text" v-model="text"/>' +
+            '<input type="button" value="Save" @click="save"/>' +
+        '</div>',
+    methods: {
+        save: function () {
+            let publication = {name: this.name, text: this.text};
+            publicationApi.save({}, publication).then(result =>
+                result.json().then(data => {
+                    this.publications.push(data);
+                    this.name = '';
+                    this.text = ''
+                })
+            )
+        }
+    }
+});
+
 Vue.component('publication-row', {
     props: ['publication'],
     template:
@@ -13,6 +41,7 @@ Vue.component('publications-list', {
     props: ['publications'],
     template:
         '<div>' +
+            '<publication-form :publications="publications"/>' +
             '<publication-row v-for="publication in publications" :key="publication.id" :publication="publication" />' +
         '</div>',
     created: function() {
