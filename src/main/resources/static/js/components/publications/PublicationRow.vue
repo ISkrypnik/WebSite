2d419ab43b86f0b14daf3e26ff989ea9a3f5b7ff
Ilya Skrypnik <ilyaskrypnik@gmail.com>
2020-06-22 16:12:27 +0700

    WS-13 Split front into vue components

---
+++ src/main/resources/static/js/components/publications/PublicationRow.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <i>({{ publication.id }})</i> Name: {{ publication.name }}. Text: {{ publication.text }}
+        <span style="position: absolute; right: 0">
+        <input type="button" value="Edit" @click="edit"/>
+        <input type="button" value="X" @click="del"/>
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['publication', 'editPublication', 'deletePublication'],
+        methods: {
+            edit() {
+                this.editPublication(this.publication)
+            },
+            del() {
+                this.deletePublication(this.publication)
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
+
+
+
+
+
---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <publications-list :publications="publications"/>
+</template>
+
+<script>
+    import PublicationsList from "../components/publications/PublicationsList.vue";
+
+    export default {
+        components: {
+            PublicationsList
+        },
+        data() {
+            return {
+                publications: []
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
