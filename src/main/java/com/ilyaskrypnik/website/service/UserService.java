d30232490dc25bd49edfa353e83f736a11a81360
Ilya Skrypnik <ilyaskrypnik@gmail.com>
2020-11-27 18:28:17 +0700

    WS-18 Add send email after success registration

--- src/main/java/com/ilyaskrypnik/website/service/UserService.java
+++ src/main/java/com/ilyaskrypnik/website/service/UserService.java
@@ -3,6 +3,8 @@ package com.ilyaskrypnik.website.service;
 import com.ilyaskrypnik.website.domain.Role;
 import com.ilyaskrypnik.website.domain.User;
 import com.ilyaskrypnik.website.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,16 +14,14 @@ import org.springframework.stereotype.Service;
 import java.util.Collections;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @Service
+@Slf4j
 public class UserService implements UserDetailsService {
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final EmailService emailService;
     private final UserRepository userRepository;
-
-    UserService(BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository) {
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        this.userRepository = userRepository;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -33,17 +33,18 @@ public class UserService implements UserDetailsService {
         User userFromDb = userRepository.findByUsername(user.getUsername());
 
         if (userFromDb != null) {
+            log.warn("Попытка регистрации пользователя с существующим username = {}.", userFromDb.getUsername());
             model.put("message", "User already exists!");
             return "registration";
         }
 
         String passwordFromFront = user.getPassword();
         user.setPassword(bCryptPasswordEncoder.encode(passwordFromFront));
-
         user.setIsActive(true);
         user.setRoles(Collections.singleton(Role.USER));
 
-        userRepository.save(user);
+        User savedUser = userRepository.save(user);
+        emailService.sendSuccessRegistrationEmail(savedUser.getId());
 
         return "redirect:/login";
     }
