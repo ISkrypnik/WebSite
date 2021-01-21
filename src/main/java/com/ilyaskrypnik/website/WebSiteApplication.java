d8fcf1e7f07fd90fd82c9b5f3074d01e63e17617
iskrypnik <ilyaskrypnik@gmail.com>
2021-01-21 21:31:07 +0700

    Reindent file with spaces

--- src/main/java/com/ilyaskrypnik/website/WebSiteApplication.java
+++ src/main/java/com/ilyaskrypnik/website/WebSiteApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebSiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebSiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebSiteApplication.class, args);
+    }
 
 }
